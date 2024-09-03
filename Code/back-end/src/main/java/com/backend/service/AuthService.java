ac5e92cb4990b14ae405d8a465cfd07efcb06323
Vitanh105 <vitanh1521@gmail.com>
2024-09-04 01:01:11 +0700

    Add signup, signin

---
+++ Code/back-end/src/main/java/com/backend/service/AuthService.java
@@ -0,0 +1,28 @@
+package com.backend.service;
+
+import com.backend.jwt.JWTUtility;
+import com.backend.model.User;
+import com.backend.repository.UserRepository;
+import com.backend.security.PasswordEncoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    private final UserRepository userRepository;
+    @Autowired
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+
+    public String loginWithEmailAndPassword(String email, String password) {
+        User user = userRepository.findByEmail(email).orElse(null);
+        assert user != null;
+        if (PasswordEncoder.getInstance().matches(password, user.getPassword())) {
+            return JWTUtility.getInstance().generateTokenWithEmail(email);
+        }
+        return null;
+    }
+}
