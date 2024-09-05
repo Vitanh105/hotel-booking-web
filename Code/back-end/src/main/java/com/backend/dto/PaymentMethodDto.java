74b87edbe7c420ae8db5c9e580e98d7de210bf2d
vanchinh9737 <vanchinh9737@gmail.com>
2024-09-05 23:47:54 +0900

    lam them cac bang City,PaymentMethod,booking,amenity,comment

---
+++ Code/back-end/src/main/java/com/backend/dto/PaymentMethodDto.java
@@ -0,0 +1,8 @@
+package com.backend.dto;
+
+import lombok.Data;
+
+@Data
+public class CityDto {
+    private String cityName;
+}
---
+++ Code/back-end/src/main/java/com/backend/specification/AmenitySpecification.java
@@ -0,0 +1,2 @@
+package com.backend.specification;public class AmenitySpecifiaction {
+}
