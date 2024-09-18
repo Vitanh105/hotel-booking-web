6b26263e1533acf3d02d97428f99b3dc58ef2d2c
Vitanh105 <vitanh1521@gmail.com>
2024-09-18 22:43:27 +0700

    Update CustomerLayout.jsx

--- Code/front-end/src/components/customer/CustomerLayout.jsx
+++ Code/front-end/src/components/customer/CustomerLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../Header'
+import Footer from '../Footer'
 
 const CustomerLayout = () => {
     return (
         <div>
             <Header />
+            <Footer />
         </div>
     )
 }
